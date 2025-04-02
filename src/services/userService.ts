56490bc011cddccca4398d10e344b0d2e43276d2
nikunjkumarparmar <parmar.nikunjkumar@gmail.com>
2025-04-02 23:40:01 +0200

    updated

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://onetouchpro.de';
+const API_URL = process.env.REACT_APP_API_URL || 'https://onetouchpro.de';
 
 export interface UserData {
     firstName: string;
